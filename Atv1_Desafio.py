c82cc0f73f720aae43e41edcd413ccef67f27803
Macasi21 <matheuscsilva000@gmail.com>
2025-06-07 18:36:35 -0300

    Add files via upload

---
+++ Atv1_Desafio.py
@@ -0,0 +1,10 @@
+
+#Verificação de idade
+#Peça ao usuario para digitar a idade, se for maior ou igual a 18 imprima "Maior de idade", caso contrario, imprima "Menor de idade"
+
+idade = int(input("Digite sua idade (apenas numeros):"))
+
+if idade >= 18:
+    print ("Maior de idade.")
+else: 
+    print("Menor de idade.")
